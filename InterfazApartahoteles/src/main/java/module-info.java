871d60dde065c306d756915cd2f15735ad4d2aeb
fermorher1337 <99404483+fermorher1337@users.noreply.github.com>
2022-12-04 19:30:38 +0100

    Delete InterfazApartahoteles directory
    F

--- InterfazApartahoteles/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.interfazapartahoteles {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.interfazapartahoteles to javafx.fxml;
-    exports com.mycompany.interfazapartahoteles;
-}
